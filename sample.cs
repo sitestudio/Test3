9cbb15f1b7238d1d433b87191034cbdd5d9a4004
Andrew <andrew@sitestudio.co>
2024-12-03 16:02:01 +1000

    comment out cs file code

--- sample.cs
+++ sample.cs
@@ -6,7 +6,7 @@ Line 3 */
 Console.WriteLine("Bye Bye World!");
 Console.WriteLine("Bye Bye World!");
 Console.WriteLine("Bye Bye World!"); // $livedoc$ testing part of comment 
-Important! This file is imp
+// Important! This file is imp
 // $livedoc$ test trest
 
 
